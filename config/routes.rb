64a2e9c0c8cf1afeac92aae68af1955e3bb26581
横溝 翔貴 <shokimac@Shokis-Mac.local>
2020-06-09 11:58:16 +0900

    Top_View_make

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get root :to => 'books#top', as: 'top'
+
+  get 'books' => 'books#index'
+
+  get 'books/:id' => 'books#show'
+
+
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get top" do
+    get books_top_url
+    assert_response :success
+  end
+
 end
